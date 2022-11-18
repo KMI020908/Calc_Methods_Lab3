7007528f367854e7e2e47d62123f51eeae787b61
KMI020908 <mikhailkochergin08@gmail.com>
2022-11-18 18:33:41 +0300

    made extropolation plot of exp

--- main.cpp
+++ main.cpp
@@ -91,7 +91,7 @@ const std::string &S_CH_ERROR_FILE_PATH, std::size_t numOfErrors = 100, Type acc
 
 template<typename Type>
 void temp_main(){
-    std::size_t numOfFinElems = 3; 
+    std::size_t numOfFinElems = 20; 
 
     checkTestLagrange(numOfFinElems, func1, -1.0, 1.0, U_IN_FILE_PATH_1, CH_IN_FILE_PATH_1, L_U_OUT_FILE_PATH_1, L_CH_OUT_FILE_PATH_1, L_U_ERROR_FILE_PATH_1, L_CH_ERROR_FILE_PATH_1);
     checkTestSpline(numOfFinElems, func1, -1.0, 1.0, S_U_OUT_FILE_PATH_1, S_CH_OUT_FILE_PATH_1, S_U_ERROR_FILE_PATH_1, S_CH_ERROR_FILE_PATH_1);
@@ -111,12 +111,29 @@ void temp_main(){
     checkTestLagrange(numOfFinElems, func6, 0.0, 2.0, U_IN_FILE_PATH_6, CH_IN_FILE_PATH_6, L_U_OUT_FILE_PATH_6, L_CH_OUT_FILE_PATH_6, L_U_ERROR_FILE_PATH_6, L_CH_ERROR_FILE_PATH_6);
     checkTestSpline(numOfFinElems, func6, 0.0, 2.0, S_U_OUT_FILE_PATH_6, S_CH_OUT_FILE_PATH_6, S_U_ERROR_FILE_PATH_6, S_CH_ERROR_FILE_PATH_6);
 
-    // Проверка ошибки экстрополяции экспоненты на [0; 2]
+    // Проверка ошибки экстрополяции экспоненты на [0; 2 + h]
     std::vector<Type> xGrid;
     std::vector<Type> fGrid;
-    getUniformGrid(xGrid, fGrid, func6, 0.0, 2.0, 10);
-    std::cout << "Значение многочлена Лагранжа в точке x = 2.2: " << LagrangePolynom(2.2, xGrid, fGrid) << '\n';
-    std::cout << "Ошибка экстрополяции в точке x = 2.2: " << std::abs(LagrangePolynom(2.2, xGrid, fGrid) - func6(2.2)) << '\n';
+    numOfFinElems = 100;
+    Type extrCoef = 4.0;
+    getUniformGrid(xGrid, fGrid, func6, 0.0, 2.0, numOfFinElems);
+    std::vector<Type> xVec, fVec;
+    Type tempX = xGrid[0];
+    Type h = (xGrid[numOfFinElems] - xGrid[0]) / (2.0 * 100);
+    while (tempX <= extrCoef * xGrid[numOfFinElems] + 1e-10){
+        xVec.push_back(tempX);
+        tempX += h;
+    }
+    getLagrangeInterpolation(xVec, fVec, xGrid, fGrid);
+    writeValueTable(xVec, fVec, "D:\\Calc_Methods\\Lab3\\exponentExtropolation.txt");
+    h = (xGrid[numOfFinElems] - xGrid[0]) / numOfFinElems;
+    std::cout << '\n';
+    std::cout << "Экстрополяция для экспоненты на равномерной сетке" << '\n';
+    std::cout << "Количество конечных элементов: " << numOfFinElems << '\n';
+    std::cout << "Шаг h = " << h << '\n';
+    std::cout << "Значение многочлена Лагранжа в точке x = 2 + h: " << LagrangePolynom(2.0 + h, xGrid, fGrid) << '\n';
+    std::cout << "Ошибка экстрополяции в точке x = 2 + h на практике: " << std::abs(LagrangePolynom(2.0 + h, xGrid, fGrid) - func6(2.0 + h)) << '\n';
+    std::cout << "Оценка ошибки экстрополяции в точке x = 2 + h в теории: " << std::pow(h, numOfFinElems+ 1) * func6(2.0 + h) << '\n';
 }
 
 int main(){
